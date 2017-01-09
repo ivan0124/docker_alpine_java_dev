79fba64937213f6a0d103d86cc68d50bceb3a33f
ivan0124 <ivan0124.tw@gmail.com>
2017-01-09 10:35:20 +0800

    add and delete files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-var ffi = require('ffi');
- 
-var libm = ffi.Library('./lib_a.so', {
-  'test_lib_a': [ 'void', [ 'int' ] ]
-});
-libm.test_lib_a(0);
- 
--- src/lib_a.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-//#include "lib_a.h"
-
-void test_lib_a(int a){
-    printf("test_lib_a() is called (in lib_a.c)\n");
-}
-
